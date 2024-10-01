c7394640056d07b395d2f2287bc50d7e42d54a1a
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-01 12:56:55 +0200

    fmt

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -49,3 +49,43 @@ Console.Write(" celsius.");
 
 // Perform Basic String Formatting in C#
 
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+Console.Write(@"c:\invoices");
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine("View English output:");
+Console.WriteLine($@"    c:\Exercise\{projectName}\data.txt");
+Console.WriteLine("");
+Console.WriteLine($"{russianMessage}:");
+Console.WriteLine($@"    c:\Exercise\{projectName}\ru-RU\data.txt");
+
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+// Perform Basic Operations on Numbers in C#
+
