8e7e197bc5b296e11e178cfa49fc08105d1a59fb
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-01 13:07:38 +0200

    basic numeric operations

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -89,3 +89,52 @@ Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 // Perform Basic Operations on Numbers in C#
 
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+
+int first = 7;
+int second = 5;
+decimal quotientB = (decimal)first / (decimal)second;
+Console.WriteLine(quotientB);
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
