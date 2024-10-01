cb62a29858127dff91c5416d11c358fb0b3fbec6
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-01 12:41:52 +0200

    types

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -3,3 +3,49 @@ Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code.");
 Console.Write("This is the first line.\nThis is the second line.");
+
+// Store and Retrieve Data Using Literal and Variable Values in C#
+
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
+
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+
+string firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+var message = "Hello world!"; // Implicit type
+
+string name = "Bob";
+int numMessages = 3;
+decimal temp = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(numMessages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
+
+// Perform Basic String Formatting in C#
+
