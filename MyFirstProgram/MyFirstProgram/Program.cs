df5a04b91ac5ffcfdb0d9227283d4c55634ecd25
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-01 12:12:04 +0200

    first prints

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -1,1 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.Write("This is the first line.\nThis is the second line.");
